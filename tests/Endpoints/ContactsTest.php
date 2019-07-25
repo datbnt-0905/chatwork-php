0c4aa6506f0326102e103991a091134f272e340e
datbnt-0905 <nguyen.tien.datb@sun-asterisk.com>
2019-07-25 10:54:30 +0700

    add Contacts endpoint

--- tests/Endpoints/ContactsTest.php
+++ tests/Endpoints/ContactsTest.php
@@ -1,5 +1,8 @@
 <?php
 
+use Mockery as m;
+
+use SunAsterisk\Chatwork\Chatwork;
 use SunAsterisk\Chatwork\Endpoints\Contacts;
 
 class StatusTest extends TestCase
@@ -7,7 +10,6 @@ class StatusTest extends TestCase
     public function testMe()
     {
         $response = $this->getMockResponse('contacts');
-
         $api = $this->getAPIMock();
         $api->shouldReceive('get')->with('contacts')->andReturns($response);
         $contacts = new Contacts($api);
